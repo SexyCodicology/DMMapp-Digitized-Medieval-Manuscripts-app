a8b9815ded1a76eb8f608b05356812658e7b1f37
giulio_menna <giulio.menna@gmail.com>
2021-09-15 13:53:55 +0200

    intial work on Library model and seeder/factory

---
+++ database/factories/LibraryFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Library;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LibraryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Library::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+
+                'nation' => $this->faker->country(),
+                'city' => $this->faker->city(),
+                'library' => $this->faker->company(),
+                'lat' => $this->faker->latitude($min = -90, $max = 90),
+                'lng' => $this->faker->longitude($min = -180, $max = 180),
+                'quantity' => $this->faker->numberBetween($min = 1000, $max = 9000),
+                'website' => $this->faker->url(),
+                'copyright' => $this->faker->sentence(),
+                'notes' => $this->faker->sentence(),
+        ];
+    }
+}
