2bbbd07656b6af8abef7d9fabe3f5555e0a2b660
giulio_menna <giulio.menna@gmail.com>
2021-09-17 11:37:40 +0200

    initial work on database migration

---
+++ database/migrations/2018_10_16_105239_create_libraries_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libraries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nation');
+            $table->string('city');
+            $table->string('library');
+            $table->string('lat');
+            $table->string('lng');
+            $table->string('quantity');
+            $table->string('website');
+            $table->string('copyright');
+            $table->string('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libraries');
+    }
+}
