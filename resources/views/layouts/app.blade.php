974b7a52a307166ef671fb09eb3a203ff127432c
Giulio Menna <giulio.menna@gmail.com>
2021-09-27 21:01:35 +0200

    incremental work on map and data pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,26 +6,27 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- TODO variable if statments: each page gets the title of the institution + digitized medieval manuscripts -->
+    {{-- TODO variable if statments: each page gets the title of the institution + digitized medieval manuscripts --}}
     <title>{{ config('app.name', 'DMMapp - Digitized Medieval Manuscripts app') }}</title>
-    <!-- TODO variable if statments: each page gets the description of the institution + digitized medieval manuscripts -->
+    {{-- TODO variable if statments: each page gets the description of the institution + digitized medieval manuscripts --}}
     <meta content="Digitized medieval manuscripts belonging to  the ..." name="description">
-    <!-- TODO check keywords -->
+    {{-- TODO check keywords --}}
     <meta content="Digitized medieval manuscripts, manuscripts, digitized books, medieval" name="keywords">
 
-    <!-- Favicons -->
+    {{-- Favicons --}}
     <link href="img/favicon.png" rel="icon">
     <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
 
-    <!-- Google Fonts -->
+    {{-- Google Fonts --}}
     <link rel="preconnect" href="https://fonts.googleapis.com/" crossorigin>
     <link rel="dns-prefetch" href="https://fonts.googleapis.com/">
     <link
         href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
         rel="stylesheet">
 
-    <!-- Vendor CSS Files -->
-    <!-- TODO convert to blade call as the CSS -->
+    {{-- Vendor CSS Files --}}
+    {{-- TODO convert to blade call as the CSS --}}
+    {{-- TODO cdn files over local files --}}
     <link href="vendor/aos/aos.css" rel="stylesheet">
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -33,7 +34,7 @@
     <link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
     <link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-    <!-- Template Main CSS File -->
+    {{-- Template Main CSS File --}}
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('css')
@@ -42,26 +43,27 @@
 
 <body>
 
-    <!-- SECTION -  Header -->
+    {{-- SECTION -  Header --}}
     <header id="header" class="fixed-top d-flex align-items-center">
         <div class="container d-flex align-items-center">
 
             <div class="logo me-auto">
                 <h1><a href="{{ url('/') }}">DMMapp</a></h1>
-                <!-- Uncomment below if you prefer to use an image logo -->
-                <a href="{{ url('/') }}"><img src="{{ asset('img/logo.png') }}" alt="DMMapp Logo"
-                        class="img-fluid"></a>
+                {{-- TODO image logo --}}
+                {{-- <a href="{{ url('/') }}"><img src="{{ asset('img/logo.png') }}" alt="DMMapp Logo"
+                        class="img-fluid"></a> --}}
             </div>
 
             <nav id="navbar" class="navbar order-last order-lg-0">
                 <ul>
+                    {{-- TODO fix nav links --}}
                     <li><a class="nav-link scrollto " href="#hero">Home</a></li>
                     <li class="dropdown"><a href="#"><span>About</span> <i class="bi bi-chevron-down"></i></a>
                         <ul>
                             <li><a class="nav-link scrollto" href="#about">About Us</a></li>
                             <li><a class="nav-link scrollto" href="#team">Team</a></li>
                             <li><a class="nav-link scrollto" href="#testimonials">Testimonials</a></li>
-                            <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i
+                            {{--<li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i
                                         class="bi bi-chevron-right"></i></a>
                                 <ul>
                                     <li><a href="#">Deep Drop Down 1</a></li>
@@ -70,7 +72,7 @@
                                     <li><a href="#">Deep Drop Down 4</a></li>
                                     <li><a href="#">Deep Drop Down 5</a></li>
                                 </ul>
-                            </li>
+                            </li>--}}
                         </ul>
                     </li>
                     <li><a class="nav-link scrollto" href="#services">Services</a></li>
@@ -80,11 +82,11 @@
                     <li class="ml-4"></li>
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
-            </nav><!-- .navbar -->
+            </nav>{{-- .navbar --}}
 
             <div class="header-social-links d-flex align-items-center">
                 <a class="nav-link" href="https://www.patreon.com/bePatron?u=3645539"
-                data-patreon-widget-type="become-patron-button">Become a Patron!</a>
+                    data-patreon-widget-type="become-patron-button">Become a Patron!</a>
                 <a href="https://twitter.com/sexycodicology" class="twitter"><i class="bi bi-twitter"></i></a>
                 <a href="https://www.facebook.com/SexyCodicology/" class="facebook"><i
                         class="bi bi-facebook"></i></a>
@@ -95,116 +97,104 @@
 
         </div>
     </header>
-    <!-- !SECTION Header -->
+    {{-- !SECTION Header --}}
 
     <main id="main">
 
-        <!-- SECTION -  Breadcrumbs -->
         @yield('breadcrumbs')
-<!--
-        <section id="breadcrumbs" class="breadcrumbs">
+        <section class="inner-page">
             <div class="container">
-                    <ol>
-                        <li><a href="index.html">Home</a></li>
-                        <li>Inner Page</li>
-                    </ol>
-                    <h2>Inner Page</h2> -->
 
-                </div>
-            </section>
-        <!-- !SECTION Breadcrumbs -->
+                @yield('content')
+            </div>
+        </section>
 
-            <section class="inner-page">
-                <div class="container">
-                    @yield('content')
-                </div>
-            </section>
+    </main>{{-- !SECTION #main --}}
 
-        </main><!-- !SECTION #main -->
+    {{-- SECTION -  Footer --}}
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
 
-        <!-- SECTION -  Footer -->
-        <footer id="footer">
-            <div class="footer-top">
-                <div class="container">
-                    <div class="row">
-
-                        <div class="col-lg-3 col-md-6">
-                            <div class="footer-info">
-                                <h3>DMMapp</h3>
-                                <div class="social-links mt-3">
-                                    <a href="https://twitter.com/sexycodicology" class="twitter"><i
-                                            class="bx bxl-twitter"></i></a>
-                                    <a href="https://www.facebook.com/SexyCodicology/" class="facebook"><i
-                                            class="bx bxl-facebook"></i></a>
-                                    <a href="https://www.linkedin.com/company/sexy-codicology" class="linkedin"><i
-                                            class="bx bxl-linkedin"></i></a>
-                                </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-info">
+                            <h3>DMMapp</h3>
+                            <div class="social-links mt-3">
+                                <a href="https://twitter.com/sexycodicology" class="twitter"><i
+                                        class="bx bxl-twitter"></i></a>
+                                <a href="https://www.facebook.com/SexyCodicology/" class="facebook"><i
+                                        class="bx bxl-facebook"></i></a>
+                                <a href="https://www.linkedin.com/company/sexy-codicology" class="linkedin"><i
+                                        class="bx bxl-linkedin"></i></a>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="col-lg-2 col-md-6 footer-links">
-                            <h4>Useful Links</h4>
-                            <ul>
-                                <li><i class="bx bx-chevron-right"></i> <a href="{{ url('/') }}">Home</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a
-                                        href="https://blog.digitizedmedievalmanuscripts.org/about/">About us</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a
-                                        href="https://blog.digitizedmedievalmanuscripts.org">Blog</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-                            </ul>
-                        </div>
+                    <div class="col-lg-2 col-md-6 footer-links">
+                        <h4>Useful Links</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="{{ url('/') }}">Home</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a
+                                    href="https://blog.digitizedmedievalmanuscripts.org/about/">About us</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a
+                                    href="https://blog.digitizedmedievalmanuscripts.org">Blog</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                        </ul>
+                    </div>
 
-                        <div class="col-lg-3 col-md-6 footer-links">
-                            <h4>Our Services</h4>
-                            <ul>
-                                <li><i class="bx bx-chevron-right"></i> <a href="{{ route('map') }}">Map</a></li>
-                                <li><i class="bx bx-chevron-right"></i> <a href="{{ route('data') }}">Data</a></li>
-                                <li><i class="bx bx-chevron-right disabled"></i> <a href="#">API (coming soon)</a></li>
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Our Services</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="{{ route('map') }}">Map</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="{{ route('data') }}">Data</a></li>
+                            <li><i class="bx bx-chevron-right disabled"></i> <a href="#">API (coming soon)</a></li>
 
-                            </ul>
-                        </div>
+                        </ul>
+                    </div>
 
-                        <div class="col-lg-4 col-md-6 footer-newsletter">
-                            <h4>Support us</h4>
-                            <p>The DMMapp lives on donations by manuscripts lovers like yourself!</p>
-                            <a href="https://www.patreon.com/bePatron?u=3645539"
-                                data-patreon-widget-type="become-patron-button">Become a Patron!</a>
-
-                        </div>
+                    <div class="col-lg-4 col-md-6 footer-patreon">
+                        <h4>Support us</h4>
+                        <p>The DMMapp lives on donations by manuscripts lovers like yourself!</p>
+                        <a href="https://www.patreon.com/bePatron?u=3645539"
+                            data-patreon-widget-type="become-patron-button">Become a Patron!</a>
 
                     </div>
+
                 </div>
             </div>
+        </div>
 
-            <div class="container">
-                <div class="copyright">
-                    &copy; Copyright <strong><span>DMMapp</span></strong>. All Rights Reserved
-                </div>
-                <div class="credits">
-                    <!-- All the links in the footer should remain intact. -->
-                    <!-- You can delete the links only if you purchased the pro version. -->
-                    <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/DMMapp-bootstrap-metro-style-template/ -->
-                    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-                </div>
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Giulio Menna</span></strong>. <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
             </div>
-        </footer>
-        <!-- !SECTION Footer -->
+            {{-- <div class="credits">
+                 All the links in the footer should remain intact.
+                 You can delete the links only if you purchased the pro version.
+                 Licensing information: https://bootstrapmade.com/license/
+                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/DMMapp-bootstrap-metro-style-template/
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div> --}}
+        </div>
+    </footer>
+    {{-- !SECTION Footer --}}
 
-        <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 
-        <!-- Vendor JS Files -->
-        <script src="vendor/aos/aos.js"></script>
-        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="vendor/glightbox/js/glightbox.min.js"></script>
-        <script src="vendor/isotope-layout/isotope.pkgd.min.js"></script>
-        <script src="vendor/swiper/swiper-bundle.min.js"></script>
-        <script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script>
+    {{-- Vendor JS Files --}}
+    <script src="vendor/aos/aos.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="vendor/swiper/swiper-bundle.min.js"></script>
+    <script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script>
 
-        <!-- Template Main JS File -->
-        <script src="js/main.js"></script>
-        @yield('javascript')
+    {{-- Template Main JS File --}}
+    <script src="js/main.js"></script>
+    @yield('javascript')
 
-    </body>
+</body>
 
-    </html>
+</html>
--- resources/views/public/map.blade.php
+++ resources/views/public/map.blade.php
@@ -3,92 +3,100 @@
 @section('css')
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.11.2/r-2.2.9/datatables.min.css" />
 @endsection
+@section('breadcrumbs')
+<section id="breadcrumbs" class="breadcrumbs">
+    <div class="container">
+        <ol>
+            <li><a href="/">Home</a></li>
+            <li>Map</li>
+        </ol>
+        <h2>Map</h2>
+    </div>
+</section>
+
+@endsection
 @section('content')
     @foreach ($libraries as $library)
     @endforeach
 
-    <div class="accordion mt-1 mb-1" id="accordionExample">
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
-                    aria-expanded="true" aria-controls="collapseOne">
-                    <strong>How does the DMMapp map work?</strong>
-                </button>
-            </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
-                data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                    The DMMapp (Digitized Medieval Manuscripts App) links to more than 500 libraries in the world. Each one
-                    of these contains digitized medieval manuscripts that can be browsed for free.
-                    <h3>How does it work?</h3>
-                    <ol>
-                        <li>Search for a library / city / country</li>
-                        <li>Click on the library you want to visit</li>
-                        <li>Click on "Browse the manuscripts" and off you go!</li>
-                    </ol>
-                    <p>The DMMapp is developed and maintained with a ton of love by the Sexy Codicology Team.</p>
-                    <h3>Love what we do?</h3>
-                    <p><a href="https://www.patreon.com/bePatron?u=3645539" data-patreon-widget-type="become-patron-button">Become a Patron!</a></p>
+    <section id="main-map" data-aos="zoom-out">
+
+        <div id="map" style="height:50em; width:100%;">
+            <noscript>
+                <div class="alert alert-info">
+                    <h4>Your JavaScript is disabled</h4>
+                    <p>Please enable JavaScript to view the map.</p>
+                </div>
+            </noscript>
+        </div>
+
+        <div id="map-table" class="mt-3">
+            <noscript>
+                <div class="alert alert-info">
+                    <h4>Your JavaScript is disabled</h4>
+                    <p>Please enable JavaScript to see the table.</p>
+                </div>
+            </noscript>
+            <table id="dmmtable" class="table table-striped table-bordered" style="width:100%; padding-bottom:1em;">
+                <thead>
+                    <tr>
+                        <th data-priority="1">Nation</th>
+                        <th>City</th>
+                        <th data-priority="2">Library</th>
+                        <th>lat</th>
+                        <th>lng</th>
+                        <th>Quantity</th>
+                        <th>Website</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+        <div class="accordion" id="accordion">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                        aria-expanded="true" aria-controls="collapseOne">
+                        <strong>How does the DMMapp map work?</strong>
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse collapsed" aria-labelledby="headingOne"
+                    data-bs-parent="#accordion">
+                    <div class="accordion-body">
+                        The DMMapp (Digitized Medieval Manuscripts App) links to more than 500 libraries in the world. Each
+                        one
+                        of these contains digitized medieval manuscripts that can be browsed for free.
+                        <h3>How does it work?</h3>
+                        <ol>
+                            <li>Search for a library / city / country</li>
+                            <li>Click on the library you want to visit</li>
+                            <li>Click on "Browse the manuscripts" and off you go!</li>
+                        </ol>
+                        <p>The DMMapp is developed and maintained with a ton of love by the Sexy Codicology Team.</p>
+                        <h3>Love what we do?</h3>
+                        <p><a href="https://www.patreon.com/bePatron?u=3645539"
+                                data-patreon-widget-type="become-patron-button">Become a Patron!</a></p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 
-    <div id="map" style="height:500px; width:100%;">
-        <noscript>
-            <div class="alert alert-info">
-                <h4>Your JavaScript is disabled</h4>
-                <p>Please enable JavaScript to view the map.</p>
-            </div>
-        </noscript>
-    </div>
+    <x-patreon />
 
-
-    <div>
-        <noscript>
-            <div class="alert alert-info">
-                <h4>Your JavaScript is disabled</h4>
-                <p>Please enable JavaScript to see the table.</p>
-            </div>
-        </noscript>
-        <table id="dmmtable" class="table table-striped table-bordered" style="width:100%; padding-bottom:1em;">
-            <thead>
-                <tr>
-                    <th data-priority="1">Nation</th>
-                    <th>City</th>
-                    <th data-priority="2">Library</th>
-                    <th>lat</th>
-                    <th>lng</th>
-                    <th>Quantity</th>
-                    <th>Website</th>
-                </tr>
-            </thead>
-            <tbody>
-
-            </tbody>
-        </table>
-    </div>
-    <div class="col-sm-4">
-        <h2>DMMapp - Digitized medieval manuscripts app</h2>
-        <p>The DMMapp (Digitized Medieval Manuscripts App) links to more than 500 libraries in the world. Each one of these
-            contains
-            digitized medieval manuscripts that can be browsed for free.</p>
-        <h3>How does it work?</h3>
-        <ol>
-            <li>Search for a library / city / country</li>
-            <li>Click on the library you want to visit</li>
-            <li>Click on "Browse the manuscripts" and off you go!</li>
-        </ol>
-
-
-    </div>
 @endsection
-<!-- Optional JavaScript -->
+{{-- Optional JavaScript --}}
 @section('javascript')
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.11.2/r-2.2.9/datatables.min.js"></script>
-    <!-- NOTE this transforms our libraries to json, which can then be read by Google maps - in dmmapp.js -->
-    <script type="text/javascript"> var libraries = {!! json_encode($libraries) !!} </script>
-    <script async type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBXbFwvj_8iz-56H2YYRdOPqxphj01fWdw&callback=initMap"></script>
+    {{-- NOTE this transforms our libraries to json, which can then be read by Google maps - in dmmapp.js --}}
+    <script type="text/javascript">
+        var libraries = {!! json_encode($libraries) !!}
+    </script>
+    <script async type="text/javascript"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBXbFwvj_8iz-56H2YYRdOPqxphj01fWdw&callback=initMap"></script>
     <script defer type="text/javascript" src="{{ asset('/js/dmmapp.js') }}"></script>
 @endsection
