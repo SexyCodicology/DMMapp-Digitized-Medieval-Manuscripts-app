f36bb99c24cadcd623b0e555f8821a527da0d30c
Giulio Menna <giulio.menna@gmail.com>
2021-09-27 20:55:16 +0200

    new patreon component

---
+++ resources/views/components/patreon.blade.php
@@ -0,0 +1,100 @@
+{{-- ======= Cta Section ======= --}}
+<section id="cta" class="cta">
+    <div class="container">
+        <div class="row" data-aos="zoom-in">
+            <div class="col-lg-9 text-center text-lg-start">
+                <h3>Support us on Patreon!</h3>
+                <p> The DMMapp is free for everyone to use, and will always be. If you would like to support the development of this application, please consider joining the Sexy Codicology on Patreon</p>
+            </div>
+            <div class="col-lg-3 cta-btn-container text-center">
+                <a class="cta-btn align-middle" href="https://www.patreon.com/SexyCodicology">Go to Patreon</a>
+            </div>
+        </div>
+
+    </div>
+</section>
+{{-- End Cta Section --}}
+
+{{-- ======= Patreon Section ======= --}}
+<section id="patreon" class="patreon section-bg">
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-3 col-md-6">
+                <div class="box" data-aos="zoom-in">
+                    <h3>Sexy Codicologists</h3>
+                    <h4><sup>€</sup>1<span> / month</span></h4>
+                    <ul>
+                        <li>Aida dere</li>
+                        <li>Nec feugiat nisl</li>
+                        <li>Nulla at volutpat dola</li>
+                        <li class="na">Pharetra massa</li>
+                        <li class="na">Massa ultricies mi</li>
+                    </ul>
+                    <div class="btn-wrap">
+                        <a href="https://www.patreon.com/join/SexyCodicology/checkout?rid=1394724"
+                            class="btn-buy">Join</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mt-4 mt-md-0">
+                <div class="box featured" data-aos="zoom-in" data-aos-delay="100">
+                    <h3>Scribes</h3>
+                    <h4><sup>€</sup>3<span> / month</span></h4>
+                    <ul>
+                        <li>Aida dere</li>
+                        <li>Nec feugiat nisl</li>
+                        <li>Nulla at volutpat dola</li>
+                        <li>Pharetra massa</li>
+                        <li class="na">Massa ultricies mi</li>
+                    </ul>
+                    <div class="btn-wrap">
+                        <a href="https://www.patreon.com/join/SexyCodicology/checkout?rid=1394644"
+                            class="btn-buy">Join</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mt-4 mt-lg-0">
+                <div class="box" data-aos="zoom-in" data-aos-delay="200">
+                    <h3>Master Scribes</h3>
+                    <h4><sup>€</sup>4.50<span> / month</span></h4>
+                    <ul>
+                        <li>Aida dere</li>
+                        <li>Nec feugiat nisl</li>
+                        <li>Nulla at volutpat dola</li>
+                        <li>Pharetra massa</li>
+                        <li>Massa ultricies mi</li>
+                    </ul>
+                    <div class="btn-wrap">
+                        <a href="https://www.patreon.com/join/SexyCodicology/checkout?rid=1394747"
+                            class="btn-buy">Join</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mt-4 mt-lg-0">
+                <div class="box" data-aos="zoom-in" data-aos-delay="300">
+                    <span class="advanced">Sponsors</span>
+                    <h3>The Pope</h3>
+                    <h4><sup>€</sup>86.50<span> / month</span></h4>
+                    <ul>
+                        <li>Aida dere</li>
+                        <li>Nec feugiat nisl</li>
+                        <li>Nulla at volutpat dola</li>
+                        <li>Pharetra massa</li>
+                        <li>Massa ultricies mi</li>
+                    </ul>
+                    <div class="btn-wrap">
+                        <a href="#" class="btn-buy">Join</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</section>
+{{-- End Patreon Section --}}
