34f2b1344492faabf8b17b66032a8628c74f1501
giulio_menna <giulio.menna@gmail.com>
2021-10-29 15:45:54 +0200

    incremental work to controller

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Library as Library;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class LibraryController extends Controller
 {
@@ -38,29 +41,26 @@ class LibraryController extends Controller
         return view('public/map', $data);
     }
 
+    //TODO do we need this function? We are searching via datatables / frontend anyway. "Searchadmin" should stay then.
+
     public function search(Request $request)
     {
         $search = $request->get('search');
+        //TODO why are we using DB here?
         $libraries = DB::table('libraries')->where('library', 'like', '%' . $search . '%')->paginate(10);
         return view('public/data', ['libraries' => $libraries]);
 
     }
 
-    public function searchadmin(Request $request)
-    {
-        $search = $request->get('search');
-        $libraries = DB::table('libraries')->where('library', 'like', '%' . $search . '%')->paginate(10);
-        return view('admin/admin', ['libraries' => $libraries]);
-
-    }
-
     public function admin()
     {
+
         $data = [];
 
-        $data['libraries'] = $this->library->paginate(10);
+        $data['libraries'] = $this->library->all();
 
         return view('admin/admin', $data);
+
     }
 
     /**
@@ -68,6 +68,8 @@ class LibraryController extends Controller
      */
     public function newLibrary(Request $request, Library $library)
     {
+        //TODO missing data entries (iiif, is_part_of, license, etc.)'
+
         $data = [];
 
         $data['nation'] = $request->input('nation');
@@ -97,62 +99,75 @@ class LibraryController extends Controller
 
                 ]
             );
-
-            $library->insert($data);
-//TODO return success message
-            return redirect('/admin');
+            try {
+                $library->insert($data);
+            } catch (Throwable $e) {
+                Log::error('Unable to add a new library to the database:' . $e);
+                //TODO check if the view has an "error" and a "success section"
+                return redirect()->route('new_library')->with("error", "Something went wrong and the library could not be saved. Check the logs.");
+            }
         }
-        $data['modify'] = 0;
-        //TODO return success message
-        return view('library/form', $data);
+
+        return redirect('new_library')->with("success", "A new institution has been successfully saved.");
+
     }
+
+    //TOOD Try-catch
 
     public function destroy($id)
     {
-        $library = Library::find($id);
-        $library->delete();
-        //TODO return success message
+        $library = Library::findOrFail($id);
 
-        return redirect('/admin');
+        try {
+            $library->delete();
+            return redirect('admin')->with("success", "An institution has been successfully deleted.");
+
+        } catch (Throwable $e) {
+
+            Log::error('Unable delete library from the database:' . $e);
+            //TODO check if the view has an "error" and a "success section"
+            return redirect()->route('new_library')->with("error", "Something went wrong and the library could not be deleted. Check the logs.");
+        }
+
     }
-
-    
 
     public function show($library_name_slug)
     {
         try {
             //NOTE find a library in the database that contains the slug in the URL
-            $library_data = Library::where('library_name_slug', $library_name_slug)->first();
-             //NOTE and now, we pass the location data AND the building data AND the alerts AND the options to the view to display it to the public.
-            return view('public.single-institution', ['library_data' => $library_data]);
+            $library_data = Library::where('library_name_slug', $library_name_slug)->firstOrFail();
+            //NOTE and now, we pass the library data to display it to the public.
 
         } catch (Throwable $e) {
-            \Log::notice('User landed on a location that has not been added to the database:' . URL::current());
-            abort(404, 'No information about this location is available at this time. ');
+            Log::notice('User landed on a institution that has not been added to the database:' . URL::current());
+            //TODO publish personalized error pages
+
+            abort(404, 'No information about this institution is available at this time. ');
         }
+        return view('public.single-institution', ['library_data' => $library_data]);
 
     }
 
     /*public function show($library_name_slug)
 
     {
-        //return __METHOD__ . ':' . $library_id;
-        $data = [];
-        $data['library_name_slug'] = $library_name_slug;
-        $data['modify'] = 1;
-        $library_data = Library::find($library_name_slug);
-        dd($library_data);
-        $data['nation'] = $library_data->nation;
-        $data['city'] = $library_data->city;
-        $data['library'] = $library_data->library;
-        $data['lat'] = $library_data->lat;
-        $data['lng'] = $library_data->lng;
-        $data['quantity'] = $library_data->quantity;
-        $data['website'] = $library_data->website;
-        $data['copyright'] = $library_data->copyright;
-        $data['notes'] = $library_data->notes;
+    //return __METHOD__ . ':' . $library_id;
+    $data = [];
+    $data['library_name_slug'] = $library_name_slug;
+    $data['modify'] = 1;
+    $library_data = Library::find($library_name_slug);
+    dd($library_data);
+    $data['nation'] = $library_data->nation;
+    $data['city'] = $library_data->city;
+    $data['library'] = $library_data->library;
+    $data['lat'] = $library_data->lat;
+    $data['lng'] = $library_data->lng;
+    $data['quantity'] = $library_data->quantity;
+    $data['website'] = $library_data->website;
+    $data['copyright'] = $library_data->copyright;
+    $data['notes'] = $library_data->notes;
 
-        return view('admin/show', $data);
+    return view('admin/show', $data);
     }8?
 
     /**
@@ -160,6 +175,10 @@ class LibraryController extends Controller
      */
 
     public function modify(Request $request, $library_id)
+
+    //TODO try catch
+    //TODO missing data entries (iiif, is_part_of, license, etc.)
+
     {
         $data = [];
         $data['library_id'] = $library_id;
@@ -175,7 +194,6 @@ class LibraryController extends Controller
         $data['notes'] = $library_data->notes;
 
         if ($request->isMethod('post')) {
-            //dd($data);
             $this->validate(
                 $request,
                 [
@@ -202,16 +220,17 @@ class LibraryController extends Controller
             $library_data->website = $request->input('website');
             $library_data->copyright = $request->input('copyright');
             $library_data->notes = $request->input('notes');
+            try {
+                $library_data->save();
+            } catch (Throwable $e) {
 
-            $library_data->save();
-            //TODO return success or fail message
-
-            return redirect('/admin');
+                Log::error('Unable to add institution to the databaase: ' . $e);
+                //TODO check if the view has an "error" and a "success section"'
+                //TODO this should redirect to the newly created insitution's page
+                return redirect()->route('new_library')->with("error", "Something went wrong and the institution could not be saved. Check the logs.");
+            }
         }
-        $data['modify'] = 1;
-        //TODO return success or fail message
-
-        return view('admin/modify', $data);
+        return redirect('admin')->with("success", "An institution has been successfully saved.");
     }
 
 }
