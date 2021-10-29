c25ba902a9c78eca8b675b434b040bbd7d732c75
giulio_menna <giulio.menna@gmail.com>
2021-10-29 15:41:16 +0200

    login link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -218,6 +218,8 @@
                         style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This
                 work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative
                     Commons Attribution 4.0 International License</a>.
+                    <div><a class="fw-lighter text-center" href="/login">login</a></div>
+
             </div>
             {{-- <div class="credits">
                  All the links in the footer should remain intact.
