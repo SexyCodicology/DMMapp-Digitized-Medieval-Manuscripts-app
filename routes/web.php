eb80eb6141abb2b2006b106118a78a1c44849aa5
giulio_menna <giulio.menna@gmail.com>
2021-10-29 15:45:35 +0200

    improvements to routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Library;
 use App\Http\Controllers\LibraryController;
+use App\Http\Controllers\RedirectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,26 +30,30 @@ Route::get('/', function () {return view('landing_page');});
 Route::get('/sample', function () {return view('index');});
 
 //SECTION Admin pannel
+//TODO "admin" is should be decleared, and not repeatedly used.
+//TODO do we need "record"? No.
 Route::middleware('auth')->group(function () {
-    Route::get('/admin', [LibraryController::class, 'admin'])->name('admin');
+    Route::prefix('admin')->group(function () {
+        Route::get('/', [LibraryController::class, 'admin'])->name('admin');
 
-    Route::get('/admin/record/new', [LibraryController::class, 'newLibrary'])->name('new_library');
-    Route::post('/admin/record/new', [LibraryController::class, 'newLibrary'])->name('create_library');
+        Route::get('/new', [LibraryController::class, 'newLibrary'])->name('new_library');
+        Route::post('/new', [LibraryController::class, 'newLibrary'])->name('create_library');
 
-    Route::get('/admin/record/edit/{library_id}', [LibraryController::class, 'modify'])->name('modify_library');
-    Route::post('/admin/record/edit/{library_id}', [LibraryController::class, 'modify'])->name('update_library');
+        Route::get('/edit/{library_id}', [LibraryController::class, 'modify'])->name('modify_library');
+        Route::post('/edit/{library_id}', [LibraryController::class, 'modify'])->name('update_library');
 
-    Route::get('/admin/search', [LibraryController::class, 'searchadmin']);
+        Route::get('/search', [LibraryController::class, 'searchadmin']);
 
-    Route::delete('/admin/record/delete/{library_id}', [LibraryController::class, 'destroy'])->name('delete_library');
+        Route::delete('/record/delete/{library_id}', [LibraryController::class, 'destroy'])->name('delete_library');
+    });
 });
 
 //SECTION public views
 Route::get('/data', [LibraryController::class, 'index'])->name('data');
 Route::get('/map', [LibraryController::class, 'dmmmap'])->name('map');
-Route::get('/{library_name_slug}', [LibraryController::class, 'show'])->name('show_library');
+Route::get('/{library:library_name_slug}', [LibraryController::class, 'show'])->name('show_library');
+//TODO do we need this search route? Most of the search is managed by datatables anyway. Maybe as an admin? Then it shouldn't be here.
 Route::get('/search', [LibraryController::class, 'search']);
 
 //SECTION redirects from old DMMapp structure
 Route::get('/record/{id}', RedirectController::class)->name('redirect');
-
