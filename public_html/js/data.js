053dce31550cc9b22b32084769098c477c57c1e1
Giulio Menna <giulio.menna@gmail.com>
2021-11-05 17:19:02 +0100

    fixes to data presentation

--- public_html/js/data.js
+++ public_html/js/data.js
@@ -4,18 +4,17 @@ $(function () {
         data: libraries,
         columns: [
             { "data": "library" }, //0
-            { "data": "iiif" }, //1
+            { "data": "library" }, //1
             { "data": "quantity" }, //2
-            { "data": "copyright" }, //3
-            { "data": "is_free_cultural_works_license" }, //4
-            { "data": "nation" }, //5
-            { "data": "city" }, //6
-            { "data": "lat" }, //7
-            { "data": "lng" }, //8
-            { "data": "notes" }, //9
+            { "data": "iiif" }, //3
+            { "data": "copyright" }, //4
+            { "data": "is_free_cultural_works_license" }, //5
+            { "data": "nation" }, //6
+            { "data": "city" }, //7
+            { "data": "lat" }, //8
+            { "data": "lng" }, //9
             { "data": "website" }, //10
-            { "data": "has_post" }, // 11
-            { "data": "post_url" } // 12
+            { "data": "library_name_slug"},
         ],
 
         responsive: true,
@@ -40,12 +39,12 @@ $(function () {
 
         columnDefs: [
             {
-                targets: [5, 6, 7, 8, 9],
+                targets: [4, 6, 7, 8, 9],
                 visible: false,
                 searchable: true
             },
             {
-                targets: [11, 10],
+                targets: [8, 9],
                 visible: false,
                 searchable: false
             },
@@ -58,46 +57,47 @@ $(function () {
                 targets: -1
             },
             {
-                targets: [1, 4, 11],
+                targets: [3, 5],
                 render: function (data) {
 
                     if (data === 1) {
-                        return "<p style='display:none'>yes</p><p class='text-center'><i class='fas fa-2x fa-check-circle text-success'></i></p>";
+                        return "<p style='display:none'>yes</p><i class='fas fa-2x fa-check-circle text-success'></i>";
 
                     }
                     else {
-                        return "<p style='display:none'>no</p><p class='text-center'><i class='fas fa-2x fa-times-circle text-danger'></i></p>";
+                        return "<p style='display:none'>no</p><i class='fas fa-2x fa-times-circle text-danger'></i>";
                     }
                 }
             },
             {
                 targets: 10,
                 render: function (data, type, row, meta) {
-                    return '<div class="d-grid gap-2"><a class="alert alert-info text-center" href="' + data + '" role="button">Browse</a></div>';
+                    return '<a class="btn btn-success" href="' + row['library_name_slug'] + '" role="button">Browse</a>';
                 }
             },
             {
-                targets: 12,
+                targets: 11,
                 render: function (data, type, row, meta) {
                     if (row['has_post'] === 1) {
-                        return '<div class="d-grid gap-2"><a class="text-center" href="' + data + '" role="button">Read <sup><i class="fas fa-external-link-alt"></i></sup></a></div>';
+                        return '<a href="' + row['post_url'] + '" role="button">Read <sup><i class="fas fa-external-link-alt"></i></sup></a>';
                     }
                     else {
-                        return '<p class="text-dark text-center" href="#" role="alert">No post available</p>';
+                        return '<p class="text-dark">No post available</p>';
                     }
                 },
             },
             {
-                targets: 0,
+                targets: 1,
                 render: function (data, type, row, meta) {
-                    return '<div class="d-grid gap-2"><a class="btn btn-primary" href="' + row['website'] + '" role="button">' + data + ' <sup><i class="fas fa-external-link-alt"></i></sup></a></div>';
+                    return '<a class="btn btn-primary" href="' + row['website'] + '" role="button">Digitized manuscripts <sup><i class="fas fa-external-link-alt"></i></sup></a>';
 
                 }
-            }, {
+            },
+            {
                 searchPanes: {
                     show: false
                 },
-                targets: [7, 8, 9, 10, 11]
+                targets: [4, 8, 9, 10, 11]
             },            {
                 targets: [0],
                 searchPanes: {
