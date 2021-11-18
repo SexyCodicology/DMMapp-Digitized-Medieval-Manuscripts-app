9d87e4bc1222851f569d7b1648fdfc32c14e58d4
giulio_menna <giulio.menna@gmail.com>
2021-11-18 23:17:06 +0100

    implemented basic sitemapping

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -4,8 +4,11 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\Contracts\Sitemapable;
 use Spatie\Sitemap\Tags\Url;
+use Carbon\Carbon;
+use App\Models\Library;
 
 class GenerateSitemap extends Command
 {
@@ -41,7 +44,30 @@ class GenerateSitemap extends Command
     public function handle()
     {
         $this->info('Generating sitemap');
-        SitemapGenerator::create('http://127.0.0.1:8000/')->writeToFile('public_html/sitemap.xml');
+
+        SitemapGenerator::create('http://127.0.0.1:8000')
+        ->writeToFile('public_html/sitemap.xml');
+
+
+        Sitemap::create()
+        ->add(Url::create('/')
+            ->setLastModificationDate(Carbon::yesterday())
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY)
+            ->setPriority(1.0))
+        ->add(Url::create('/data')
+            ->setLastModificationDate(Carbon::yesterday())
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY)
+            ->setPriority(0.9))
+
+        ->add(Url::create('/map')
+            ->setLastModificationDate(Carbon::yesterday())
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY)
+            ->setPriority(0.8))
+
+        ->add(Library::all())
+
+        ->writeToFile('public_html/sitemap.xml');
+
         $this->info('Sitemap generated');
         return Command::SUCCESS;
     }
