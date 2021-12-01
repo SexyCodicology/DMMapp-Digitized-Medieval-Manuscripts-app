9f4daa160705d3a4fcc7a89b4b7b67b3bdc11fa2
Giulio Menna <giulio.menna@gmail.com>
2021-12-01 18:31:20 +0100

    make sure local and staging pages are not indexed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,9 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    @env(['local', 'staging'])
+    <meta name="robots" content="none" />
+    @endenv
 
     {{-- Primary Meta Tags --}}
     <title>@if (View::hasSection('title'))@yield('title')@else{{ config('app.name', 'Digitized Medieval Manuscripts app - DMMapp') }}@endif</title>
