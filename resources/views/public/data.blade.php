c3f4779e4d2f6a14c425171a710039b454512196
Giulio Menna <Dioscorides@users.noreply.github.com>
2023-02-12 10:20:11 +0100

    harmonize reporting among views

--- resources/views/public/data.blade.php
+++ resources/views/public/data.blade.php
@@ -119,17 +119,37 @@
     </div>
     <div class="row my-3">
         <div class="col text-center">
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfP_TNstBIoCI9mBhA81cN7XxASGx4cLknBOuyp44Tm7Qh9_g/viewform"
-               rel="noopener" class="btn btn-warning" target="_blank" role="button" aria-pressed="true">
-                <i class="bi-exclamation-circle"></i> Report data issue <sup><i
-                        class="bi-box-arrow-up-right fa-xs"></i></sup>
-            </a>
             <p class="small text-muted mt-4">The DMMapp data is crowdsourced. Help us keep it up-to-date by reporting
                 inaccuracies.</p>
         </div>
     </div>
 
     <x-patreon/>
+
+    <div class="position-fixed bottom-0 start-50 translate-middle-x" style="z-index: 11">
+        <div id="liveToast" class="toast show" role="alert" aria-live="assertive" aria-atomic="true"
+             data-bs-autohide="false">
+            <div class="toast-header">
+                <i class="bi bi-check-double"></i>
+                <strong class="me-auto ms-2">Improve the DMMapp!</strong>
+                <strong class="me-auto"></strong>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body text-center">
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfP_TNstBIoCI9mBhA81cN7XxASGx4cLknBOuyp44Tm7Qh9_g/viewform"
+                   rel="noopener" class="btn btn-warning" target="_blank" role="button" aria-pressed="true">
+                    <i class="bi bi-exclamation-circle-fill"></i> Report data issue <sup><i
+                            class="bi bi-box-arrow-up-right fa-xs"></i></sup>
+                </a>
+                <a href="https://docs.google.com/forms/d/1EvEN3Ctzt1rQgGPPcyyAZ4UcSN3p-aqVqfOIUTE75Xk"
+                   rel="noopener"
+                   class="btn btn-danger mt-2" target="_blank" role="button" aria-pressed="true">
+                    <i class="bi bi-exclamation-circle-fill"></i> Report missing institution <sup><i
+                            class="bi bi-box-arrow-up-right fa-xs"></i></sup>
+                </a>
+            </div>
+        </div>
+    </div>
 
 @endsection
 {{-- Optional JavaScript --}}
