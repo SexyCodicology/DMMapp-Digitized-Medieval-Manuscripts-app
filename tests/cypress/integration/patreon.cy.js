d2537707652328cfe280c51fd3502f2b20f8315f
Giulio Menna <Dioscorides@users.noreply.github.com>
2023-02-13 17:52:42 +0100

    cypress tests

--- tests/cypress/integration/patreon.cy.js
+++ tests/cypress/integration/patreon.cy.js
@@ -1,19 +1,20 @@
 describe('check Patreon links across app', () => {
-    it('passes if all links to Patreon work', () => {
-        cy.visit('/');
-        cy.get('#header > .container > .header-social-links > .patreon').should('have.attr', 'href', 'https://www.patreon.com/bePatron?u=3645539');
-        cy.get('.footer-top > .container > .row > .col-lg-4 > a').should("have.attr", 'href', 'https://www.patreon.com/bePatron?u=3645539');
-        cy.get('.col-lg-3 > .footer-info > .social-links > .patreon').should("have.attr", 'href', 'https://www.patreon.com/bePatron?u=3645539');
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('checks Patreon links in layouts.app', () => {
+        cy.get('[data-dmmapp="patreon-top-icon"]').should("have.attr", 'href', 'https://www.patreon.com/join/424150');
+        cy.get('[data-dmmapp="patreon-footer-icon"]').should("have.attr", 'href', 'https://www.patreon.com/join/424150');
+        cy.get('[data-dmmapp="patreon-footer-icon"]').should("have.attr", 'href', 'https://www.patreon.com/join/424150');
 
     })
-    it('passes if individual links to individual subscriptions to Patreon work', () => {
-        cy.visit('/');
-        cy.get('[data-cy="patreon-cta"]').should("have.attr", 'href', 'https://www.patreon.com/SexyCodicology')
-        cy.get('[data-cy="patreon-sexy-codicologists"]').should("have.attr", 'href', 'https://www.patreon.com/join/424150')
-        cy.get('[data-cy="patreon-scribes"]').should("have.attr", 'href', 'https://www.patreon.com/join/424150')
-        cy.get('[data-cy="patreon-master-scribes"]').should("have.attr", 'href', 'https://www.patreon.com/join/424150')
-        cy.get('[data-cy="patreon-pope"]').should("have.attr", 'href', 'https://www.patreon.com/join/424150')
-
+    it('checks working links in Patreon component', () => {
+        cy.get('[data-dmmapp="patreon-cta"]').should("have.attr", 'href', 'https://www.patreon.com/SexyCodicology')
+        cy.get('[data-dmmapp="patreon-sexy-codicologists"]').should("have.attr", 'href', 'https://www.patreon.com/join/424150')
+        cy.get('[data-dmmapp="patreon-scribes"]').should("have.attr", 'href', 'https://www.patreon.com/join/424150')
+        cy.get('[data-dmmapp="patreon-master-scribes"]').should("have.attr", 'href', 'https://www.patreon.com/join/424150')
+        cy.get('[data-dmmapp="patreon-pope"]').should("have.attr", 'href', 'https://www.patreon.com/join/424150')
 
     })
 })
