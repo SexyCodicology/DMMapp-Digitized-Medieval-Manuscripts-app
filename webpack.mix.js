679a8b0c726e87a6c0a6e72991feec2100bb13bb
Giulio Menna <Dioscorides@users.noreply.github.com>
2023-02-18 12:13:21 +0100

    required js

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,6 +19,7 @@ mix.js('resources/js/app.js', 'public_html/js')
     .minify('resources/js/data.js', 'public_html/js/data.min.js')
     .minify('resources/js/dashboard.js', 'public_html/js/dashboard.min.js')
     .minify('resources/js/main.js', 'public_html/js/main.min.js')
+    .minify('resources/js/map-data.js', 'public_html/js/map-data.min.js')
 
     .sass('resources/sass/app.scss', 'public_html/css')
 
