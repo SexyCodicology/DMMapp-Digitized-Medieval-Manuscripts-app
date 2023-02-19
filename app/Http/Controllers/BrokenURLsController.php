d668fea6c72b36a48f2871612e91159b2b6aa79c
Giulio Menna <Dioscorides@users.noreply.github.com>
2023-02-19 12:01:42 +0100

    remove yajra datatables from brokenURL

--- app/Http/Controllers/BrokenURLsController.php
+++ app/Http/Controllers/BrokenURLsController.php
@@ -5,14 +5,10 @@ namespace App\Http\Controllers;
 use App\Jobs\CheckWebsitesInDatabaseJob;
 use App\Models\BrokenLink;
 use App\Models\BrokenLinksTask;
-use Exception;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Yajra\DataTables\Facades\DataTables;
 
 class BrokenURLsController extends Controller
 {
@@ -20,27 +16,14 @@ class BrokenURLsController extends Controller
     /**
      * Check all the URLs in the database and add broken links to a table
      *
-     * @param Request $request
-     * @return Application|Factory|View|JsonResponse
-     * @throws Exception
+     * @return Application|Factory|View
      */
 
-    public function index(Request $request)
+    public function index(): View|Factory|Application
     {
         //dd($BrokenLinks);
-        if ($request->ajax()) {
-            $data = BrokenLink::all();
-            return Datatables::of($data)
-                ->addIndexColumn()
-                ->addColumn('action', function($row){
-                    return  '<a class="btn btn-primary" href="/admin/edit/' . $row['id'] . '" role="button">Edit</a>';
-                })
-                ->rawColumns(['action'])
-                ->make();
-
-        }
-
-        return view('admin.broken_links');
+        $brokenLinks = BrokenLink::all();
+        return view('admin.broken_links', compact('brokenLinks'));
     }
 
     public function executeJob(): RedirectResponse
